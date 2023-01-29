2a148165e2282dfff3558d220a6888208c90ec3e
Eddy555 <stevenkipping@gmail.com>
2023-01-29 13:16:08 +0000

    Use time prefs for 24h or 12h setting

--- app/datetime.js
+++ app/datetime.js
@@ -6,6 +6,7 @@ const weekNumElem = document.getElementById("weekNumText");
 const dElem = document.getElementById("dateText");
 const hmElem = document.getElementById("hoursMinutesText");
 //const sElem = document.getElementById("secondsText");
+import { preferences } from "user-settings";
 
 let weekText = document.getElementById("weekText");
 weekText.groupTransform.rotate.angle = -90;
@@ -16,7 +17,11 @@ export function updateDisplay(now) {
   
   //Time
   let hours = now.getHours();
-  hours = util.zeroPad(hours);
+  if (preferences.clockDisplay === "12h") {
+    hours = (hours + 24) % 12 || 12;
+  } else {
+    hours = util.zeroPad(hours);
+  }
   let mins = util.monoDigits(now.getMinutes());
   //let secs = util.monoDigits(now.getSeconds());
   hmElem.text = hours + ':' + mins;
